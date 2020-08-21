4c7ace73044b2e0d60c4b3d11d7c49fec1165569
Mikee Jazmines <mikee.jazmines@gmail.com>
2020-08-21 13:33:09 +0800

    Sync files

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
+import IconButton from "@material-ui/core/IconButton";
+
+function Header() {
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon className="header__icon" fontSize="large" />
+            </IconButton>
+            <img
+                className="header__logo"
+                src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+                alt="tinder logo"
+            />
+            <IconButton>
+                <ForumIcon className="header__icon" fontSize="large" />
+            </IconButton>
+        </div>
+    );
+}
+
+export default Header;
