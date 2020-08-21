d3e90c1aa4b733fe237679ce39dd8de346378a78
Mikee Jazmines <mikee.jazmines@gmail.com>
2020-08-21 17:32:08 +0800

    Add buttons at the bottom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import TinderCards from "./TinderCards";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SwipeButtons from "./SwipeButtons";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           </Route>
           <Route path="/">
             <TinderCards/>
+            <SwipeButtons/>
           </Route>
         </Switch>
       </Router>
---
+++ src/SwipeButtons.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./SwipeButtons.css";
+import ReplayIcon from "@material-ui/icons/Replay";
+import CloseIcon from "@material-ui/icons/Close";
+import StarRateIcon from "@material-ui/icons/StarRate";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+import IconButton from "@material-ui/core/IconButton";
+
+const SwipeButtons = () => {
+  return (
+    <div className="swipeButtons">
+      <IconButton className="swipeButtons__repeat">
+        <ReplayIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButtons__left">
+        <CloseIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButtons__star">
+        <StarRateIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButtons__right">
+        <FavoriteIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButtons__lightning">
+        <FlashOnIcon fontSize="large" />
+      </IconButton>
+    </div>
+  );
+};
+
+export default SwipeButtons;
