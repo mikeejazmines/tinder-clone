536e30d0fa6bfd5207f1b3f5e237446d66fc5a06
Mikee Jazmines <mikee.jazmines@gmail.com>
2020-08-21 13:42:03 +0800

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import Header from "./Header";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-    return (
-        <div className="App">
-            {/* Header */}
-            <Header />
-
-            {/* Tinder Cards */}
-            {/* Buttons below tinder cards */}
-
-            {/* Chats screen */}
-            {/* Individual chat screen */}
-        </div>
-    );
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/chat">
+            <h1>I am on the chatpage</h1>
+          </Route>
+          <Route path="/">
+            <h1>I am on the homepage</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -5,21 +5,21 @@ import ForumIcon from "@material-ui/icons/Forum";
 import IconButton from "@material-ui/core/IconButton";
 
 function Header() {
-    return (
-        <div className="header">
-            <IconButton>
-                <PersonIcon className="header__icon" fontSize="large" />
-            </IconButton>
-            <img
-                className="header__logo"
-                src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
-                alt="tinder logo"
-            />
-            <IconButton>
-                <ForumIcon className="header__icon" fontSize="large" />
-            </IconButton>
-        </div>
-    );
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon className="header__icon" fontSize="large" />
+      </IconButton>
+      <img
+        className="header__logo"
+        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+        alt="tinder logo"
+      />
+      <IconButton>
+        <ForumIcon className="header__icon" fontSize="large" />
+      </IconButton>
+    </div>
+  );
 }
 
 export default Header;
